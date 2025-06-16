86bb11eb977dcfc354b7565ccb785f9bb82599a4
yanisse354 <11525@holbertonstudents.com>
2025-06-16 14:39:09 +0000

    add all change

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+/*
+ * File: 5-printf.c
+ * Auth: smail mohammed 
+ */
+
+ #include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
