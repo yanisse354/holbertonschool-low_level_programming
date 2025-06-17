0df7e788eafb2de1de0cc089897f344cbde28870
yanisse354 <11525@holbertonstudents.com>
2025-06-17 13:15:20 +0000

    add all change

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
