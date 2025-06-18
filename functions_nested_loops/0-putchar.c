c0af584dd658cc28c718d4a88879b5c23dcba373
yanisse354 <11525@holbertonstudents.com>
2025-06-18 10:49:28 +0000

    add all change

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-putchar.c
+ * Auth: mohammed
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+
+	return (0);
+}
