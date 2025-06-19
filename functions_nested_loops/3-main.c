57e4ae7c37fcba87e9ed19acc6e3104a53570b0d
yanisse354 <11525@holbertonstudents.com>
2025-06-19 12:54:25 +0000

    add all change

--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -12,7 +12,7 @@ int main(void)
     r = _islower('H');
     _putchar(r + '0');
     r = _islower('o');
-    _putchar(r + '0');
+    _putchar(r + '1');
     r = _islower(108);
     _putchar(r + '0');
     _putchar('\n');
