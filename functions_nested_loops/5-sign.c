982549a531675f0f1362ab772100704cb8e47cdd
yanisse354 <11525@holbertonstudents.com>
2025-06-19 16:40:06 +0000

    add all change

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,7 +9,9 @@
  * print_sign - function that prints the sign of a number.
  * @n: The character to be checked.
  *
- * Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero, -1 and prints - if n is less than zero.
+ * Return: 1 and prints + if n is greater than zero, 
+ * 0 and prints 0 if n is zero, 
+ * -1 and prints - if n is less than zero.
  */
 
 int print_sign(int n)
