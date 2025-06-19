7ddbecd1078b95e5bfcee70793f5b8940302a1e4
yanisse354 <11525@holbertonstudents.com>
2025-06-19 20:46:08 +0000

    add all change

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number.
+ * @n: The character to be checked.
+ *
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	_putchar(n + '0');
+		return (n);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,6 +6,7 @@ void print_alphabet_x10(void);
 int _isalpha(int c);
 int _islower(int c);
 int print_sign(int n);
+int print_last_digit(int);
 int _abs(int);
 int _putchar(char);
 
