1cba7c0915ca475960acdd6b71b16df106664478
yanisse354 <11525@holbertonstudents.com>
2025-06-20 12:40:25 +0000

    add all change

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -16,6 +16,9 @@ void print_to_98(int n)
 {
 	while (n <= 98)
 	{
+		if(n < 0)
+			n *= -1;
+
 		if (n >= 10)
 		{
 			_putchar((n / 10) + '0');
@@ -25,7 +28,7 @@ void print_to_98(int n)
 		{
 			_putchar(n + '0');
 		}
-		
+
 		if (n != 98)
 		{
 			_putchar(',');
