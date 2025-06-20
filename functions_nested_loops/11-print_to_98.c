90e6f503569b67310141d798307931f9fb931a78
yanisse354 <11525@holbertonstudents.com>
2025-06-20 12:20:52 +0000

    add all change

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98
+ * followed by a new line.
+ *
+ * Return: the natural numbers 98
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		if (n >= 10)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		else
+		{
+			_putchar(n + '0');
+		}
+		
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		n++;
+	}
+
+	_putchar('\n');
+}
