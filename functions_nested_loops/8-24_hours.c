45b2099a99b465403d4a7cf9147f31c435523c31
yanisse354 <11525@holbertonstudents.com>
2025-06-20 09:44:26 +0000

    add all change

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - function that prints every minute of the day of 
+ * jack_bauer - function that prints every minute of the day of
  * Jack Bauer, starting from 00:00 to 23:59.
  * @n: The character to be checked.
  *
