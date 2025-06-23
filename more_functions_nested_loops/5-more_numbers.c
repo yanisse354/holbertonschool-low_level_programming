d964d5a3453036f81bee8ed90f159c101a6254a7
yanisse35 <11525@holbertonstudents.com>
2025-06-23 13:50:22 +0000

    add all change

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -21,8 +21,8 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			if (num >= 10)
-				_putchar(num / 10 + '0');
-			_putchar(num % 10 + '0');
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 
 	_putchar('\n');
