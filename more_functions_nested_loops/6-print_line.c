f014af5a5ce67d3b4c8879c650e41dee97589200
yanisse35 <11525@holbertonstudents.com>
2025-06-23 14:27:22 +0000

    add all change

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-print_line.c
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal.
+ *@n: The number of _ characters to be printed.
+ */
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+
+	_putchar('_');
+}
