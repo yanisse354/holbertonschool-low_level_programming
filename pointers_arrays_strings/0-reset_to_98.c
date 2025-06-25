7ca212d4d03a297a5c664c34387ed5f2255be153
yanisse354 <11525@holbertonstudents.com>
2025-06-25 10:27:09 +0000

    add all change

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter 
+ * and updates the value it points  to 98.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+int _putchar(char);
+void reset_to_98(int *n);
+
+#endif
