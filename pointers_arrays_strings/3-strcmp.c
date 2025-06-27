dac79f9020984b13b6105fd9839a566553b4bc79
yanisse354 <11525@holbertonstudents.com>
2025-06-27 14:17:18 +0000

    add all change

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+/*
+ * File: non du fichier
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: chaine de caractere n1
+ * @s2: chaine de caractere n2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
