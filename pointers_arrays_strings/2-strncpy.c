f678b98502755d4965bb28705d477b8bed1c960b
yanisse354 <11525@holbertonstudents.com>
2025-06-27 12:54:05 +0000

    add all change

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+/*
+ * File: 2-strncpy.c
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ * @n: to coppy
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j < n )
+	{
+		dest[i] = src[j];
+		
+		i++;
+		j++;
+	}
+	return (dest);
+}
