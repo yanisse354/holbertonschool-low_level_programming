2e933689dcf2748a911b57a226c9e87c9175f24b
yanisse354 <11525@holbertonstudents.com>
2025-06-29 15:15:05 +0000

    add all change

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+/*
+ * File: nom du fichier
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337.
+ * @str: pointer of chain string
+ *
+ * return: string into 1337
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char t1[] = "aAeEoOtTlL";
+	char t2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+    	{
+        	for (j = 0; t1[j] != '\0'; j++)
+        	{	
+            		if (str[i] == t1[j])
+            		{
+                		str[i] = t2[j];
+            		}
+        	}
+    	}
+
+    return (str);
+}
