8514691539465e4fc98e06d0930e96c8bdf6bce0
yanisse354 <11525@holbertonstudents.com>
2025-07-01 20:14:20 +0000

    add all change

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+/*
+ * File: nom du fichier 
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * _memcpy -  function that copies memory area.
+ * @n: function copies
+ * @src: bytes from memory area
+ * @dest: to memory area
+ *
+ * Return: pointer dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = dest[i];
+	}
+
+	return (dest);
+}
