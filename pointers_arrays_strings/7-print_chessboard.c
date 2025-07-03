797da58c7df28471ca7fa323c0b5c486744949fe
yanisse354 <11525@holbertonstudents.com>
2025-07-03 13:38:43 +0000

    add all change

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+/*
+ * File: nom du fichier
+ * Auth: mohammed
+ */
+
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @(*a)[8]; pointeure vers un tableau 2d
+ *
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
