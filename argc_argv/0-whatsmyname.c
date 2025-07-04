1e47a090d73a59dfe93a27ed6e8fa5f34f7008e5
yanisse354 <11525@holbertonstudents.com>
2025-07-04 10:38:55 +0000

    add all change

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: mohammed
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
