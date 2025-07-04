a456891e9272c39b5659254f297fc9584c82fe21
yanisse354 <11525@holbertonstudents.com>
2025-07-04 18:26:26 +0000

    add all change

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -16,7 +16,6 @@
 
 int _pow_recursion(int x, int y)
 {
-
 	if (y < 0)
 	{
 		return (-1);
@@ -26,5 +25,4 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	return (x * _pow_recursion(x, y - 1));
-
 }
