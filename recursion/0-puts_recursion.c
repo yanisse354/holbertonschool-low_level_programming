e97352eb34b8a20d39e76c06f6982dd7d3e1a33e
yanisse354 <11525@holbertonstudents.com>
2025-07-04 16:03:00 +0000

    add all change

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,8 +6,12 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string, followed by a new line.
- * @s: pointe to an string.
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: The string to be printed
+ *
+ * Description: This function prints a string recursively,
+ * using the _putchar function. It prints one character at a time
+ * followed by a newline when the end of the string is reached.
  */
 
 void _puts_recursion(char *s)
