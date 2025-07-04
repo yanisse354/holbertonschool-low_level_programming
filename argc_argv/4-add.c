a469888fd74e7d415fd1d5bbd11ac4813a56a497
yanisse354 <11525@holbertonstudents.com>
2025-07-04 13:00:41 +0000

    add all change

---
+++ argc_argv/4-add.c
@@ -0,0 +1,20 @@
+/*
+ * File: nom du fichier
+ * Auth: mohammed
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If no number is passed to the program, print 0,
+ *  If one of the number contains symbols that are not digits, 
+ * print Error and return 1
+ *
+ */
+int main(int argc, char *argv[])
+	
