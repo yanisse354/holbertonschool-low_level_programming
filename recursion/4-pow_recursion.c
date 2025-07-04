ed22266c20dc5ca9061b8b95d048e3fc47641aa7
yanisse354 <11525@holbertonstudents.com>
2025-07-04 18:24:40 +0000

    add all change

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -6,7 +6,8 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y.
+ * _pow_recursion - function that returns the value of x raised 
+ * to the power of y.
  * @y: value
  * @x: value
  *
