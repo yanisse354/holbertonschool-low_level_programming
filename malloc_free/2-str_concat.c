5cfc6abfe4e7cb2fff6d034760381b0a49dedb11
yanisse354 <11525@holbertonstudents.com>
2025-07-07 17:34:01 +0000

    add all change

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -31,8 +31,8 @@ char *str_concat(char *s1, char *s2)
 	size_t j = 0;
 	char *result;
 
-	size_t len1 = s1 ? strlen(s1) : 0;
-	size_t len2 = s2 ? strlen(s2) : 0;
+	size_t len1 = s1 ? my_strlen(s1) : 0;
+	size_t len2 = s2 ? my_strlen(s2) : 0;
 
 	result = (malloc(len1 + len2 + 1));
 	if (!result)
